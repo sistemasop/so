17d6d2949f73b2042b918e8dd40a33a6820ba0c9
sistemasop <ingridgcalderon@outlook.com>
2014-05-13 13:18:33 -0300

    Agregué el método cargar
    Tiene un error pero se encuentra la idea principal con comentarios de lo
    que hay que modificar

--- TpSistemasOperativos/src/MemoriaPrincipal.py
+++ TpSistemasOperativos/src/MemoriaPrincipal.py
@@ -3,11 +3,16 @@ class MemoriaPrincipal:
     def __init__ (self):
         self.cantidadDeCeldas = 1024
         self.celdas = []
+        self.proximaACargar = celdas[0]
 
     def cargar(programa)
-    #Hacer un for en la coleccion de instrucciones
-    #de un programa
-    	__celdas = 
+    '''Hacer un for en la coleccion de instrucciones
+    de un programa'''
+    	for instruccion in programa.instrucciones
+    		celdas[proximaACargar] = (__proximaACargar, instruccion) #Esto esta mal pero la idea es que en cada celda de 
+    																 # Memoria se guarde la direccion de Memoria y su
+    																 # instruccion asociada 
+    		__proximaACargar = __proximaACargar + 1
 
 
 
