06822784ee007d6287697ae9c0306e10d2593bfb
sistemasop <ingridgcalderon@outlook.com>
2014-05-13 16:44:59 -0300

    Test MemoriaPrinciapl
    Hay que correrlo

--- TpSistemasOperativos/tests/TestMemoriaPrincipal.py
+++ TpSistemasOperativos/tests/TestMemoriaPrincipal.py
@@ -1,14 +1,28 @@
 import unittest
-
+from Instruccion import *
+from Programa import *
+from MemoriaPrincipal import *
 
 class Test(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+    	instruccion1 = Instruccion('instruccion1')
+    	instruccion2 = Instruccion('instruccion2')
+    	instruccion3 = Instruccion('instruccion3')
+
+    	programa = Programa('programa1')
+    	programa.addIns(instruccion1)
+    	programa.addIns(instruccion2)
+    	programa.addIns(instruccion3)
+
+    	memoria = Memoria()
+
+
 
     def testName(self):
-        pass
+        memoria.cargar(programa)
+        self.assertEquals(memoria.__proximaAcargar, celdas[3], 'se cargo correctamente')
 
 
 if __name__ == "__main__":
